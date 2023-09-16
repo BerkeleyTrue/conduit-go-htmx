253f63426e0bfcc74d80cacac02300ad0d8675b0
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-09-16 16:37:58 -0700

    fix(views): make links dynamic

--- internal/app/controllers/main.go
+++ internal/app/controllers/main.go
@@ -7,6 +7,41 @@ import (
 type (
 	Controller struct {
 	}
+	Link struct {
+		Uri   string
+		Title string
+	}
+)
+
+var (
+	UnAuthedLinks = []Link{
+		{
+			Uri:   "/",
+			Title: "Home",
+		},
+		{
+			Uri:   "/login",
+			Title: "Login",
+		},
+		{
+			Uri:   "/register",
+			Title: "Sign up",
+		},
+	}
+	AuthedLinks = []Link{
+		{
+			Uri:   "/",
+			Title: "Home",
+		},
+		{
+			Uri:   "/editor",
+			Title: "New Article",
+		},
+		{
+			Uri:   "/settings",
+			Title: "Settings",
+		},
+	}
 )
 
 func NewController() *Controller {
@@ -15,8 +50,29 @@ func NewController() *Controller {
 
 func RegisterRoutes(app *fiber.App, c *Controller) {
 	app.Get("/", c.Index)
+	app.Get("/login", c.Login)
+	app.Get("/register", c.Register)
 }
 
 func (c *Controller) Index(ctx *fiber.Ctx) error {
-	return ctx.Render("index", fiber.Map{}, "layouts/main")
+	return ctx.Render("index", fiber.Map{
+		"Links": UnAuthedLinks,
+		"Page":  ctx.Path(),
+	}, "layouts/main")
+}
+
+func (c *Controller) Login(ctx *fiber.Ctx) error {
+	return ctx.Render("auth", fiber.Map{
+		"IsLogin": true,
+		"Links":   UnAuthedLinks,
+		"Page":    ctx.Path(),
+	}, "layouts/main")
+}
+
+func (c *Controller) Register(ctx *fiber.Ctx) error {
+	return ctx.Render("auth", fiber.Map{
+		"IsLogin": false,
+		"Links":   UnAuthedLinks,
+		"Page":    ctx.Path(),
+	}, "layouts/main")
 }
