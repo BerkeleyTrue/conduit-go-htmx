002618fda77a3310e293a088197b9e585e0d588e
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-09-20 00:10:51 -0600

    fix(models): remove old directories
    not required

--- internal/domain/models/directory.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type DirectoryStat struct {
-	IsDirectory bool
-	Name        string // name of directory to show user
-	Uri         string // the uri (link) to the directory
-	Image       string // preview image of directory
-}
