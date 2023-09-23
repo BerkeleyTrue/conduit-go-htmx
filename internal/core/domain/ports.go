ca08d3b3e0e70ac09192c89109642c0591d4ed60
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-09-22 21:10:24 -0600

    feat(core): define ports

--- internal/core/domain/ports.go
+++ internal/core/domain/ports.go
@@ -1,19 +1,53 @@
 package domain
 
 type (
+	Updater[T any] func(*T) *T
 	UserRepository interface {
-	  GetByID(id string) (*User, error)
-	  GetByEmail(email string) (*User, error)
-	}
-	ArticleRepository interface {
-	}
-	CommentRepository interface {
+		Create(password string) (*User, error)
+		GetByID(id string) (*User, error)
+		GetByEmail(email string) (*User, error)
+		GetByUsername(username string) (*User, error)
+		Update(userId string, updater Updater[*User]) (*User, error)
+		Follow(userId, authorId string) (*User, error)
+		Unfollow(userId, authorId string) (*User, error)
 	}
 
-	UserService interface {
+	ArticleCreateInput struct {
+		title       string
+		description string
+		body        string
+		tags        []string
+		authorId    string
 	}
-	ArticleService interface {
+
+	ArticleListInput struct {
+		tag       string
+		author    string // authorId
+		favorited string // authorId
+		limit     int8
+		offset    int8
 	}
-	CommentService interface {
+
+	ArticleRepository interface {
+		Create(input ArticleCreateInput) (*Article, error)
+		GetById(articleId string) (*Article, error)
+		GetBySlug(mySlug string) (*Article, error)
+		List(input ArticleListInput) []*Article
+		Update(slug string, updater Updater[*Article]) (*Article, error)
+		Delete(slug string) error
+	}
+
+	CommentCreateInput struct {
+		body      string
+		authorId  string // UserId
+		articleId string // ArticleId
+	}
+
+	CommentRepository interface {
+		Create(input CommentCreateInput) (*Comment, error)
+		GetById(commentId string) (*Comment, error)
+		GetByArticleId(articleId string) ([]*Comment, error)
+		Update(commentId string, updater Updater[*Comment]) (*Comment, error)
+		Delete(commentId string) error
 	}
 )
