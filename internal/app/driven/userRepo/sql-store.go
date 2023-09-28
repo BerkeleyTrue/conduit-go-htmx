c96940423e49103b6db993c737d6b9413289872a
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-09-28 14:21:45 -0600

    feat(app/userRepo): flush out sql store

--- internal/app/driven/userRepo/sql-store.go
+++ internal/app/driven/userRepo/sql-store.go
@@ -29,6 +29,14 @@ var (
         created_at TEXT NOT NULL,
         updated_at TEXT NOT NULL
     );
+
+    CREATE TABLE IF NOT EXISTS followers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        follower_id INTEGER NOT NULL,
+        created_at TEXT NOT NULL,
+        UNIQUE(user_id, follower_id)
+    );
   `
 	Module = fx.Options(
 		fx.Provide(NewSqlStore),
@@ -56,46 +64,151 @@ func NewSqlStore(db *sqlx.DB) *SqlStore {
 	}
 }
 
-func (s *SqlStore) Create(input domain.UserCreateInput) (*domain.User, error) {
-  now := time.Now()
-  user := domain.User{
-    Username: input.Username,
-    Email: input.Email,
-    Password: input.HashedPassword,
-    Bio: "",
-    Image: "",
-    CreatedAt: now,
+// get followers for a user
+func (s *SqlStore) getFollowers(userId string) ([]string, error) {
+  var followers []string
+  err := s.db.Select(&followers, "SELECT follower_id FROM followers WHERE user_id = $1", userId)
+
+  if err != nil {
+    return nil, err
   }
 
-  query := `
+  return followers, nil
+}
+
+func (s *SqlStore) Create(input domain.UserCreateInput) (*domain.User, error) {
+	now := time.Now()
+	user := domain.User{
+		Username:  input.Username,
+		Email:     input.Email,
+		Password:  input.HashedPassword,
+		Bio:       "",
+		Image:     "",
+		CreatedAt: now,
+	}
+
+	query := `
     INSERT INTO users (username, email, password, bio, image, created_at, updated_at)
     VALUES (:username, :email, :password, :bio, :image, :created_at, :updated_at)
   `
-  _, err := s.db.NamedExec(query, user)
+	_, err := s.db.NamedExec(query, user)
 
-  return &user, err
+	return &user, err
 }
 
 func (s *SqlStore) GetByID(id string) (*domain.User, error) {
-	panic("implement me")
+	var user domain.User
+	err := s.db.Get(&user, "SELECT * FROM users WHERE id = $1 LIMIT 1", id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (s *SqlStore) GetByEmail(email string) (*domain.User, error) {
-	panic("implement me")
+	var user domain.User
+	err := s.db.Get(&user, "SELECT * FROM users WHERE email = $1 LIMIT 1", email)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (s *SqlStore) GetByUsername(username string) (*domain.User, error) {
-	panic("implement me")
+	var user domain.User
+	err := s.db.Get(&user, "SELECT * FROM users WHERE username = $1 LIMIT 1", username)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
-func (s *SqlStore) Update(userId string, updater domain.Updater[domain.User]) (*domain.User, error) {
-	panic("implement me")
+func (s *SqlStore) Update(
+	userId string,
+	updater domain.Updater[domain.User],
+) (*domain.User, error) {
+	var user domain.User
+	err := s.db.Get(&user, "SELECT * FROM users WHERE id = $1 LIMIT 1", userId)
+	if err != nil {
+		return nil, err
+	}
+	updatedUser := updater(&user)
+
+	_, err = s.db.NamedExec(`
+    UPDATE users
+    SET username = :username,
+        email = :email,
+        password = :password,
+        bio = :bio,
+        image = :image,
+        updated_at = :updated_at
+    WHERE id = :id
+  `, updatedUser)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedUser, nil
 }
 
 func (s *SqlStore) Follow(userId, authorId string) (*domain.User, error) {
-	panic("implement me")
+  var author domain.User
+  err := s.db.Get(&author, "SELECT * FROM users WHERE id = $1 LIMIT 1", authorId)
+
+  if err != nil {
+    return nil, err
+  }
+
+  _, err = s.db.Exec(`
+    INSERT INTO followers (user_id, follower_id)
+    VALUES ($1, $2)
+    WHERE id = $2
+  `, userId, authorId)
+
+  if err != nil {
+    return nil, err
+  }
+
+  followers, err := s.getFollowers(authorId)
+
+  if err != nil {
+    return nil, err
+  }
+  author.Followers = followers
+
+  return &author, nil
 }
 
 func (s *SqlStore) Unfollow(userId, authorId string) (*domain.User, error) {
-	panic("implement me")
+  var author domain.User
+  err := s.db.Get(&author, "SELECT * FROM users WHERE id = $1 LIMIT 1", authorId)
+
+  if err != nil {
+    return nil, err
+  }
+
+  _, err = s.db.Exec(`
+    DELETE FROM followers
+    WHERE user_id = $1 AND follower_id = $2
+  `, userId, authorId)
+
+  if err != nil {
+    return nil, err
+  }
+
+  followers, err := s.getFollowers(authorId)
+
+  if err != nil {
+    return nil, err
+  }
+
+  author.Followers = followers
+
+  return &author, nil
 }
