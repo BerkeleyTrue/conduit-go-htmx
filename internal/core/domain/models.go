6df1007db6c5dab2c709ecbd44bfbd8feb3b877b
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-10-03 14:23:02 -0600

    feat(domain/User): pretty print, hide password

--- internal/core/domain/models.go
+++ internal/core/domain/models.go
@@ -1,6 +1,8 @@
 package domain
 
 import (
+	"fmt"
+
 	"github.com/berkeleytrue/conduit/internal/infra/data/krono"
 	"github.com/berkeleytrue/conduit/internal/infra/data/password"
 	"github.com/berkeleytrue/conduit/internal/infra/data/slug"
@@ -43,3 +45,24 @@ type (
 		UpdatedAt string `json:"updatedAt"`
 	}
 )
+
+func (u *User) String() string {
+	return fmt.Sprintf(`
+User{
+  UserId: %d, 
+  Username: %s, 
+  Email: %s,
+  Password: ***, 
+  Bio: %s,
+  Image: %s, 
+  CreatedAt: %s, 
+  UpdatedAt: %s
+}`, u.UserId,
+		u.Username,
+		u.Email,
+		u.Bio,
+		u.Image,
+		u.CreatedAt,
+		u.UpdatedAt,
+	)
+}
