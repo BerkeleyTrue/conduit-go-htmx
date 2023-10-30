e4154a2e03dbecdf7d0ffba7e672240c65512114
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-10-29 20:20:57 -0600

    fix(drivers/edit-articles): add publish trigger

--- internal/app/drivers/article-edit.go
+++ internal/app/drivers/article-edit.go
@@ -3,11 +3,13 @@ package drivers
 import "github.com/gofiber/fiber/v2"
 
 func (c *Controller) getEditArticle(fc *fiber.Ctx) error {
+  slug := fc.Params("slug")
 	props := editArticleProps{
 		layoutProps: getLayoutProps(fc),
 	}
 
 	props.layoutProps.title = "Edit Article"
+	props.isNew = slug == ""
 
-	return renderComponent(editArticle(props), fc)
+	return renderComponent(editArticleComp(props), fc)
 }
