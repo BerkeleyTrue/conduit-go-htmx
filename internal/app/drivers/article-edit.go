254c11789b144013f2f4630a45f2866bd7ddbcde
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-10-31 16:09:14 -0600

    feat(drivers/edit-article): populate fields on edit

--- internal/app/drivers/article-edit.go
+++ internal/app/drivers/article-edit.go
@@ -3,13 +3,27 @@ package drivers
 import "github.com/gofiber/fiber/v2"
 
 func (c *Controller) getEditArticle(fc *fiber.Ctx) error {
-  slug := fc.Params("slug")
+	slug := fc.Params("slug")
 	props := editArticleProps{
 		layoutProps: getLayoutProps(fc),
 	}
 
+	if slug == "" {
+		props.layoutProps.title = "New Article"
+		props.isNew = true
+		return renderComponent(editArticleComp(props), fc)
+	}
+
 	props.layoutProps.title = "Edit Article"
-	props.isNew = slug == ""
+
+	article, err := c.articleService.GetBySlug(fc.Context(), slug, 0)
+
+	if err != nil {
+		c.log.Debug("Error getting article", "error", err)
+		return fc.Redirect("/", fiber.StatusNotFound)
+	}
+
+	props.article = &article
 
 	return renderComponent(editArticleComp(props), fc)
 }
